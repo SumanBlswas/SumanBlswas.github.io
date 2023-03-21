b0309a6285ec4facf1e96814dd81b9efc322e980
Suman Biswas <sumanmanner@gmail.com>
2023-03-21 13:16:10 +0530

    i

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -111,17 +111,6 @@ const About = () => {
           </Box>
         </Flex>
       </Center>
-
-      <Button
-        id="resume-button-2"
-        as="a"
-        href={Resume}
-        target="_blank"
-        download="Suman_Biswas_Resume.pdf"
-        onClick={downloadResume}
-      >
-        Resume
-      </Button>
       {/* <Button id="resume-button-2">
         <a
           id="resume-link-2"
@@ -134,6 +123,17 @@ const About = () => {
           Resume
         </a>
       </Button> */}
+      <Button
+        id="resume-button-2"
+        as="a"
+        href={Resume}
+        target="_blank"
+        rel="noreferrer"
+        download="Suman_Biswas_Resume.pdf"
+        onClick={downloadResume}
+      >
+        Resume
+      </Button>
     </Box>
   );
 };
